9e17ad18c772d7e6f2381d9c392e400f3df8a9e1
orin <orinelbilia@gmail.com>
2024-08-21 20:08:46 +0300

    added the delete route

--- personal_achievement_tracker/src/controllers/achievements_controller.py
+++ personal_achievement_tracker/src/controllers/achievements_controller.py
@@ -1,18 +1,34 @@
-from bson import ObjectId
-
 from src.models.achievement import Achievement
 from datetime import datetime
 from config.database import get_db
+from fastapi import APIRouter, HTTPException
 
 db = get_db()
 
 
-async def create_achievement(achievement: Achievement):
+async def create_achievement(achievement: Achievement) -> Achievement:
     achievement_data = achievement.model_dump(exclude={"id"})
     achievement_data["created_at"] = datetime.now()
     result = db.achievements.insert_one(achievement_data)
+    if not result:
+        raise HTTPException(status_code=500, detail="Failed to retrieve the created achievement")
     return db.achievements.find_one({"_id": result.inserted_id})
 
 
-async def get_achievement(achievement_id: str):
-    return db.achievements.find_one({"_id": achievement_id})
+async def get_achievement(achievement_id: str) -> Achievement:
+    achievement = db.achievements.find_one({"_id": achievement_id})
+    if not achievement:
+        raise HTTPException(status_code=404, detail="Achievement not found")
+    return achievement
+
+
+async def delete_achievement(achievement_id: str) -> str:
+    result = db.achievements.delete_one({"_id": achievement_id})
+    if result.deleted_count:
+        return "achievement deleted successfully"
+    raise HTTPException(status_code=404, detail="Achievement not found")
+
+
+async def update_achievement(achievement_id: str) -> int:
+    result = db.achievements.delete_one({"_id": achievement_id})
+    return result.deleted_count
--- personal_achievement_tracker/src/routes/achievements_routes.py
+++ personal_achievement_tracker/src/routes/achievements_routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from src.models.achievement import Achievement
 from config.database import get_db
-from src.controllers.achievements_controller import create_achievement
+from src.controllers.achievements_controller import create_achievement, get_achievement, delete_achievement, \
+    update_achievement
 
 router = APIRouter()
 
@@ -9,18 +10,21 @@ db = get_db()
 
 
 @router.post('/achievements/', response_model=Achievement)
-async def create_achievement(achievement: Achievement):
-    new_achievement = create_achievement(achievement)
-    if not new_achievement:
-        raise HTTPException(status_code=500, detail="Failed to retrieve the created achievement")
-    return new_achievement
+async def create_achievement(achievement: Achievement) -> Achievement:
+    return await create_achievement(achievement)
 
 
-@router.get('/achievements/{achievement_id}')
-async def get_achievement(achievement_id: str):
-    achievement = get_achievement(achievement_id)
-    if not achievement:
-        raise HTTPException(status_code=404, detail="Achievement not found")
-    return achievement
+@router.get('/achievements/{achievement_id}', response_model=Achievement)
+async def get_achievement(achievement_id: str) -> Achievement:
+    return await get_achievement(achievement_id)
 
 
+@router.delete('/achievements/{achievement_id}', response_model=str)
+async def delete_achievement(achievement_id: str) -> str:
+    return await delete_achievement(achievement_id)
+
+
+@router.post('/achievements/{achievement_id}')
+async def update_achievement(achievement_id: str):
+    return await update_achievement(achievement_id)
+   
